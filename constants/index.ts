f27eb1e4d1ef676905fa71b5e726135ffaa30108
mhassanrahi <m.hassan.rahi@gmail.com>
2024-10-20 14:35:35 +0500

    added constants

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const APP_NAME = "InvoiceAsaan";
+
+export const AVAILABLE_STATUSES = [
+  {
+    id: "pending",
+    label: "Pending",
+  },
+  {
+    id: "paid",
+    label: "Paid",
+  },
+];
